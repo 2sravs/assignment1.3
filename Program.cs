323057f412f947fea7cb14fe8ec0e98329f3dba9
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-12 15:19:56 +0530

    assignment

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conassign1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = "30";
+                Console.WriteLine(input);
+                int tryParsedInt;
+                int.TryParse(input, out tryParsedInt);
+                Console.WriteLine("tryparsed number is: \t" + tryParsedInt);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
